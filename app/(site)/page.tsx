358cfe648b86c7a400fed1e1e1aeebe2f2e1164a
Ifeanyi Osi-Okeke <ifeanyi.osi@sourcemygadgets.com>
2023-05-21 01:04:38 +0100

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image src={logo} alt="logo" height={48} width={48} className="mx-auto w-auto" />
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account!</h2>
       </div>
       {/* Auth form */}
       <AuthForm />
